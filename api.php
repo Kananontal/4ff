9bd538c5da1e3e34c811470a71f297b896bfbfaf
Kananontal <s6102041620038@email.kmutnb.ac.th>
2021-09-02 00:50:28 +0700

    update

--- api.php
+++ api.php
@@ -1,35 +1,35 @@
 <?php
-	include './control/connection.php';
-	header("Access-Control-Allow-Origin: *");
+    include 'connected.php';
+    header("Access-Control-Allow-Origin: *");
 
-if (!$conn) {
+if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    
+
     exit;
 }
 
-if (!$conn->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $conn->error);
+if (!$link->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $link->error);
     exit();
-	}
+    }
 
 if (isset($_GET)) {
 
-	$n=100; 
-		function getName($n) { 
-			$characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
-			$randomString = ''; 
-		  
-			for ($i = 0; $i < $n; $i++) { 
-				$index = rand(0, strlen($characters) - 1); 
-				$randomString .= $characters[$index]; 
-			} 
-		  
-			return $randomString; 
-		}
-	 $randomtoken = getName($n);
+    $n=50; 
+        function getName($n) { 
+            $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
+            $randomString = ''; 
+
+            for ($i = 0; $i < $n; $i++) { 
+                $index = rand(0, strlen($characters) - 1); 
+                $randomString .= $characters[$index]; 
+            } 
+
+            return $randomString; 
+        }
+     $randomtoken = getName($n);
 
 
 
@@ -43,34 +43,35 @@ if (isset($_GET)) {
 
 
 
-	if ($_GET['isAdd'] == 'true') {
+    if ($_GET['isAdd'] == 'true') {
 
 
-		$name = $_GET['name'];
-		$lastname = $_GET['lastname'];
-		$email = $_GET['email'];
-		$username = $_GET['username'];
-		$password = $_GET['password'];
-		$token = $_GET['token'];
-		$phone = $_GET['phone'];
-		
-							
-		$conn = "INSERT INTO `member`(`id`, `name`, `lastname`, `email`, `username`, `password`, `token`, `phone`, `user_status`, `point`) VALUES (Null,'$name','$lastname','$email','$username','$password','$randomtoken','$phone','member','0')";
+        $name = $_GET['name'];
+        $lastname = $_GET['lastname'];
+        $email = $_GET['email'];
+        $username = $_GET['username'];
+        $password = $_GET['password'];
+        $token = $_GET['token'];
+        $phone = $_GET['phone'];
+
+    $hash = password_hash("$password",PASSWORD_BCRYPT);
+
+        $sql = "INSERT INTO `member` (`id`, `username`, `password`, `name`, `lastname`, `email`, `token`, `user_status`) VALUES (null, '$name', '$lastname', '$email', '$username', '$hash', '$token', '$phone','member')";
 
 
 
 
 
-		$result = mysqli_query($link, $sql);
+        $result = mysqli_query($link, $sql);
 
-		if ($result) {
-			echo "true";
-		} else {
-			echo "false";
-		}
+        if ($result) {
+            echo "true";
+        } else {
+            echo "false";
+        }
 
-	} else echo "Welcome nattapoom";
-   
+    } else 
+{}
 }
-	mysqli_close($link);
+    mysqli_close($link);
 ?>
